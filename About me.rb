68cce776c7e13f587d75882a5a3d99930bcac72d
Xiomara Romine <xiomararomine@comcast.net>
2014-04-19 17:50:44 -0400

    updated blog to include site map and styled pages

---
+++ About me.rb
@@ -0,0 +1,6 @@
+<H1>About me</H1>
+<ul>
+<li>I'm artistic</li>
+<li>I know basic HTML</li>
+<li>I live in Chattanooga</li>
+</ul>
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'sitemap/index'
+
+  get 'aboutme/index'
+
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
